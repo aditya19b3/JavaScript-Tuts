563e03c8e8b8acfd73fae128b57f859e671826c4
aditya19b3 <66126493+aditya19b3@users.noreply.github.com>
2023-11-14 09:20:08 +0000

    variables in JS

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountID = 144553
+let accountEmail = "aditya@gmail.com"
+var accountPassword = "12345"
+accountCity = "Bilsi"
+let accountState;
+
+// accountID = 2
+accountEmail = "new@gmail.com"
+accountPassword = "54321"
+accountCity = "Mumbai"
+
+console.log(accountID);
+// console.log(accountEmail);
+// console.log(accountPassword);
+
+console.table([accountID,accountEmail,accountPassword,accountCity,accountState]);
+
