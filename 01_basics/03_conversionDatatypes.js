9fac347e25f4e50d9c8fa6e67437214e93d3267c
aditya19b3 <66126493+aditya19b3@users.noreply.github.com>
2023-11-15 20:28:25 +0000

    type Conversion in JS

---
+++ 01_basics/03_conversionDatatypes.js
@@ -0,0 +1,47 @@
+// let score = "33abc"
+// let score = null
+let score = undefined
+// let score = true
+// let score = "Aditya"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+
+// console.log(typeof (valueInNumber));
+// console.log(valueInNumber);
+
+
+//"33" = 33
+// "33abc" = Nan
+// "true" = 1
+// null = 0
+// undefined = NaN
+
+let isLoggedIn = 1
+// let isLoggedIn = 0
+// let isLoggedIn = ""
+// let isLoggedIn = "Aditya"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(typeof isLoggedIn);
+
+// console.log(typeof booleanIsLoggedIn);
+
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false; "Aditya" => true
+
+
+let someNumber = 44
+
+let stringNumber = String(someNumber)
+
+console.log(typeof someNumber);
+
+console.log(typeof stringNumber);
+
+console.log(stringNumber);
