4843ecd9affcf81f59913294c350c4b181e4b68e
aditya19b3 <66126493+aditya19b3@users.noreply.github.com>
2023-11-15 20:49:25 +0000

    some Basic operations.

---
+++ 01_basics/03_Operations.js
@@ -0,0 +1,42 @@
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "Hello"
+let str2 = " Aditya"
+let str3 = str1 + str2
+
+console.log(str3);
+
+// console.log("1" + 2);
+// console.log("1" + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+// console.log(3 + 4 * 5 % 3); // use prenthesis in real world projects
+
+
+// console.log(true);
+// console.log(+true);
+// console.log(true+);
+// console.log(+"");
+
+// let num1, num2, num3
+
+// num1 = num2 = num3 = 2 + 2
+
+// console.log(num1);
+// console.log(num2);
+// console.log(num3);
+
+let gameCounter = 100
+// gameCounter++;
+++gameCounter;
+
+console.log(gameCounter);
