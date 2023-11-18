36c1cb3ecfa74f30191de60b716326600a7183ed
aditya19b3 <66126493+aditya19b3@users.noreply.github.com>
2023-11-18 08:49:13 +0000

    functions part 2

---
+++ 03_basics/02_functions.js
@@ -0,0 +1,16 @@
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(200, 400, 500, 600, 700));
+
+const user = {
+    username: "Aditya",
+    prices: 199
+}
+
+function handleObject(anyobject){
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+handleObject(user)
